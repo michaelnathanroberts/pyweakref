e6814eef56bea1c09925068843eef75d24e81899
michaelnathanroberts <81385815+michaelnathanroberts@users.noreply.github.com>
2021-03-25 15:35:31 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'pyweakref',        
+  packages = ['pyweakref'],   
+  version = '0.1',     
+  license='MIT',        
+  description = 'Pure Python implementation of weak references',   
+  author = 'Michael Roberts',                 
+  author_email = 'michaelnathanroberts@gmail.com',      
+  url = 'https://github.com/user/michaelnathanroberts',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['python 3'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.9',
+  ],
+)
